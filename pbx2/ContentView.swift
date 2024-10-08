61dda53a28c795f59d8873bc041824425be39409
srkimdev <emirated3858@gmail.com>
2024-10-08 14:16:49 +0900

    init

---
+++ pbx2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  pbx2
+//
+//  Created by 김성률 on 10/8/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
---
+++ pbx2/pbx2App.swift
@@ -0,0 +1,17 @@
+//
+//  pbx2App.swift
+//  pbx2
+//
+//  Created by 김성률 on 10/8/24.
+//
+
+import SwiftUI
+
+@main
+struct pbx2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
